1e7c4661d4bd87cd11e66300459385be7b59dbbd
Jono <jono@foodnotblogs.com>
2009-03-16 22:43:50 +0000

    * Added a method for external templating. Now quicknail can be used from within an existing PHP page. Now any php script in the same directory can use quicknail. An example of how to do this can be found in _custom.php * Added _index.php so people can use quicknail as their index. Just rename it to index.php

---
+++ _custom.php
@@ -0,0 +1,18 @@
+<? $qncustom = true; include ('qnail.php'); ?>
+
+<html>
+<head>
+
+<? print $qnout[head]; ?>
+
+</head>
+<body bgcolor=lightgreen>
+
+begin outer page
+
+<? echo $qnout[body]; ?>
+
+end outer page
+
+</body>
+</html>
